e1fa942eb13064e7fde68cbe4103c871d65c75c9
zhongzhiqiang <zqzhong1211@163.com>
2018-06-27 14:12:47 +0800

    refactor: modify code structure.

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# Geass
+# Zeass
 A node full-stack framework based on koa2.
-
-```
-所有人的GEASS能力是在获得GEASS那一时刻，自己内心最深处的想要实现的愿望。
-```
 
 ## TODO
 
---
+++ src/zeass.js
@@ -0,0 +1,12 @@
+import Controller from './base/controller';
+import Service from './base/service';
+import helper from './helper';
+import middleware from './middleware';
+
+export default {
+  Controller,
+  Service,
+  helper,
+  middleware
+};
+
