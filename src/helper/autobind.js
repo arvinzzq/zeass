e7a277b48c1f93e0a872d8ffecb53610f6eeecc4
zz1211 <zqzhong1211@163.com>
2018-07-05 20:07:58 +0800

    feat: add autobind to bind this to class instance.

--- src/helper/autobind.js
+++ src/helper/autobind.js
@@ -1,7 +1,6 @@
 /**
  * Return a descriptor removing the value and returning a getter
  * The getter will return a .bind version of the function
- * and memoize the result against a symbol on the instance
  */
 function boundMethod(target, key, descriptor) {
   const fn = descriptor.value;
@@ -9,12 +8,11 @@ function boundMethod(target, key, descriptor) {
   if (typeof fn !== 'function') {
     throw new Error(`@autobind decorator can only be applied to methods not: ${typeof fn}`);
   }
-
   return {
     configurable: true,
     get() {
       return fn.bind(this);
-    },
+    }
   };
 }
 
@@ -22,28 +20,15 @@ function boundMethod(target, key, descriptor) {
  * Use boundMethod to bind all methods on the target.prototype
  */
 function boundClass(target) {
-  // (Using reflect to get all keys including symbols)
-  let keys;
-  // Use Reflect if exists
-  if (typeof Reflect !== 'undefined' && typeof Reflect.ownKeys === 'function') {
-    keys = Reflect.ownKeys(target.prototype);
-  } else {
-    keys = Object.getOwnPropertyNames(target.prototype);
-    // use symbols if support is provided
-    if (typeof Object.getOwnPropertySymbols === 'function') {
-      keys = keys.concat(Object.getOwnPropertySymbols(target.prototype));
-    }
+  let keys = Object.getOwnPropertyNames(target.prototype);
+  if (typeof Object.getOwnPropertySymbols === 'function') {
+    keys = keys.concat(Object.getOwnPropertySymbols(target.prototype));
   }
-
   keys.forEach((key) => {
-    // Ignore special case target method
     if (key === 'constructor') {
       return;
     }
-
     const descriptor = Object.getOwnPropertyDescriptor(target.prototype, key);
-
-    // Only methods need binding
     if (typeof descriptor.value === 'function') {
       Object.defineProperty(target.prototype, key, boundMethod(target, key, descriptor));
     }
